d0d7e169b2ad7d00c1e0cd16897510cd6e6a189b
Diego Auza Derpic <diego.auza.d@gmail.com>
2024-03-23 16:56:00 -0400

    Add flowbite to improve some styles.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import 'flowbite';
+// import 'flowbite-datepicker';
+import 'flowbite-datepicker-vanilla';
+// import 'flowbite/dist/datepicker.turbo.js';
 
 // Timezone detection and submission
 // Detect current time zone and load the value inside a hidden form.
@@ -15,3 +19,14 @@ document.addEventListener("turbo:load", () => {
     }
 });
 
+window.document.addEventListener("turbo:render", (_event) => {
+    window.initFlowbite();
+});
+
+// or
+
+window.document.addEventListener("turbo:submit-end", (_event) => {
+    window.setTimeout(() => {
+        window.initFlowbite();
+    }, 10);
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,8 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
+pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.turbo.min.js"
+pin "flowbite-datepicker-vanilla", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/datepicker.min.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
