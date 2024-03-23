d9fb3b645f93ed75c8b052e57397321ce4cafc7d
Diego Auza Derpic <diego.auza.d@gmail.com>
2024-03-23 15:39:18 -0400

    WIP: Detect Timezone automatically

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  def set_timezone
+    if params[:timezone].present? && session[:timezone] != params[:timezone]
+      session[:timezone] = params[:timezone]
+      # Redirect or render only if timezone is updated
+      redirect_to(request.referrer || root_path)
+    else
+      # Prevent loop by not redirecting or rendering
+      head :no_content
+    end
+  end
+
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Timezone detection and submission
+// Detect current time zone and load the value inside a hidden form.
+document.addEventListener("turbo:load", () => {
+    const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const currentTimezone = document.body.getAttribute('data-timezone');
+
+    // Only update the timezone if it has changed
+    if (userTimezone !== currentTimezone) {
+        document.getElementById('user_timezone').value = userTimezone;
+        document.getElementById('timezone_form').requestSubmit();
+    }
+});
+
