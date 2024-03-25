c4246b09725586f7f355142f37dc842bb32f3120
Diego Auza Derpic <diego.auza.d@gmail.com>
2024-03-25 18:26:37 -0400

    Change db system from SQLite to PostgreSQL.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_03_23_005345) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.datetime "start_time"
